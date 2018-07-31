c07d923c71db176ecb6b44285695de9035ffab6a
Isaac Wong <isaac@thirdcaptain.com>
2018-07-31 03:55:32 +0000

    Initial commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""creates an archive from web_static"""
+
+from fabric.api import local
+from datetime import datetime
+from pathlib import Path
+
+time = datetime.now()
+year = time.strftime("%Y")
+month = time.month
+day = time.day
+hour = time.hour
+minute = time.minute
+second = time.second
+filename = "web_static_{}{}{}{}{}{}".format(year, month,
+                                            day, hour, minute, second)
+command = "tar -cvzf versions/{}.tgz web_static".format(filename)
+
+def do_pack():
+    """creates an archive"""
+    local("mkdir -p versions")
+    local(command)
+    my_file = Path("versions/{}.tgz".format(filename))
+    if my_file.is_file():
+        return "versions/{}.tgz".format(filename)
+    else:
+        return None
