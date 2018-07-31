b0bafca0999227b4ea81bb357c290bb1d0658668
Isaac Wong <isaac@thirdcaptain.com>
2018-07-31 00:04:47 +0000

    Initial commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,42 @@
 #!/usr/bin/env bash
-# this script  sets up your web servers for the deployment of web_static
-# update and install nginx
-sudo apt-get -y update
-sudo apt-get -y install nginx
-# create folders
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-# create simple file to test
-echo -e '<html>\n<head>\nHBnB</head>\n<body>\nHolberton School\n</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html
-# create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-# give ownership to the group and user
+# setup web server to deploy web_static
+
+# Install server
+#sudo apt-get update -y
+#sudo apt-get install nginx -y
+
+# Make web directories
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+
+# Make simple HTML file
+sudo touch /data/web_static/releases/test/index.html
+sudo echo -e "<html>
+<body>
+This is a website
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic links
+LINK="/data/web_static/current"
+if [ -e $LINK ]; then
+   # echo "File $LINK exists."
+   sudo rm /data/web_static/current
+   sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+else
+   # echo "File $LINK does not exist."
+   sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+fi
+
+# Change ownership
 sudo chown -R ubuntu:ubuntu /data/
-# creating alias
-sudo sed -i "37i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n\n" /etc/nginx/sites-enabled/default
+
+# Alias hbnb_static
+ALIAS='location \/hbnb_static\/ {\n                 alias \/data\/web_static\/current\/;\n                 autoindex off;\n         }\n'
+sudo sed -i "30i$ALIAS" /etc/nginx/sites-available/default
+
+# Restart nginx service
 sudo service nginx restart
