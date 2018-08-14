2ec575dedfb4eee460873314ff64e75bc60f5ddb
Isaac Wong <Isaac Wong>
2018-08-14 01:54:24 +0000

    Pep8 fixes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""Flask web app that prints Hello HBNB!"""
 
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
+    """returns Hello HBNB!"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
