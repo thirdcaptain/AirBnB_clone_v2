de9c74077005c88cd91cc265d889c976f98f6cfa
Isaac Wong <Isaac Wong>
2018-08-15 03:21:46 +0000

    Initial commit

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -10,16 +10,27 @@ def hello():
     """returns Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return "HBNB"
+
 
 @app.route("/c/<text>")
 def c_text(text):
     """shows c and text"""
     text = text.replace("_", " ")
     return "C %s" % text
+
+
+@app.route("/python", defaults={"text":"is cool"})
+@app.route("/python/", defaults={"text":"is cool"})
+@app.route("/python/<text>")
+def python_text(text):
+    """shows python and text"""
+    text = text.replace("_", " ")
+    return "Python %s" % text
 
 if __name__ == '__main__':
     app.run()
