51511c2f744bf43f9c5a9c106e93f1c727989522
Isaac Wong <339@holbertonschool.com>
2018-08-17 03:23:48 +0000

    Removed extra new() from __init__

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+#            models.storage.new(self)
         if "created_at" in kwargs:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
